75176718b11ab384caaa860854f1b031024b2baf
evan <evan.exe@gmail.com>
2011-10-26 00:18:03 -0400

    +s

--- src/mesh.js
+++ src/mesh.js
@@ -87,7 +87,7 @@ Mesh = function(options) {
     this.addVertexBuffer('vertices', 'gl_Vertex');
     if (options.coords) this.addVertexBuffer('coords', 'gl_TexCoord');
     if (options.normals) this.addVertexBuffer('normals', 'gl_Normal');
-    if (options.color) this.addVertexBuffer('color', 'gl_Color');
+    if (options.color) this.addVertexBuffer('colors', 'gl_Color');
     if (!('triangles' in options) || options.triangles) this.addIndexBuffer('triangles');
     if (options.lines) this.addIndexBuffer('lines');
 };
